ab130286f363baeea056bc5803fb65c476806b2c
Krasimira Georgieva <krassi77bg@gmail.com>
2017-07-04 00:14:47 +0300

    Delete incomplete solution.

--- Programming Fundametals - Exam Preparation I/03. Endurance Rally/03. Endurance Rally.cs
+++ Programming Fundametals - Exam Preparation I/03. Endurance Rally/03. Endurance Rally.cs
@@ -42,49 +42,12 @@ namespace _03.Endurance_Rally
                         Console.WriteLine($"{driver} - reached {i}");
                         break;
                     }
-
                 }
                 if (fuel > 0)
                 {
                     Console.WriteLine($"{driver} - fuel left {fuel:f2}");
                 }
             }
-
-
-
-
-            /*zadacha 3  judge 70/100
-
-            string[] players = Regex.Split(Console.ReadLine(), @"\s+");
-            double[] route = Regex.Split(Console.ReadLine(), @"\s+").Select(p => double.Parse(p) * -1).ToArray();
-            int[] checkpointIndexStr = Regex.Split(Console.ReadLine(), @"\s+").Where(t => t.Length > 0).Select(int.Parse).ToArray();
-
-            foreach (var i in checkpointIndexStr)
-            {
-                if (i >= 0 && i < route.Length)
-                {
-                    route[i] *= -1;
-                }
-            }
-            foreach (var p in players)
-            {
-                var fuel = (double)p[0];
-                var index = 0;
-                foreach (var r in route)
-                {
-                    fuel += r;
-                    if (fuel <= 0)
-                    {
-                        Console.WriteLine("{0} - reached {1}", p, index);
-                        break;
-                    }
-                    index++;
-                }
-                if (fuel > 0)
-                {
-                    Console.WriteLine("{0} - fuel left {1:F2}", p, fuel);
-                }
-            }*/
         }
     }
 }
