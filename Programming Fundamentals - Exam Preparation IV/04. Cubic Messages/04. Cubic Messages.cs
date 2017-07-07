47e480dd2f6e2b3de4cb0cf2bc78bffbbdb6c663
Krasimira Georgieva <krassi77bg@gmail.com>
2017-07-07 11:25:38 +0300

    Add 04.Cubic Messages solution.

---
+++ Programming Fundamentals - Exam Preparation IV/04. Cubic Messages/04. Cubic Messages.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace _04.Cubic_Messages
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "Over!")
+                {
+                    break;
+                }
+
+                int count = int.Parse(Console.ReadLine());
+                var regex = new Regex($@"^(\d+)([a-zA-Z]{{{count}}})([^a-zA-Z]*)$");
+
+                var match = regex.Match(line);
+                if (match.Success)
+                {
+                    var left = match.Groups[1].Value;
+                    var message = match.Groups[2].Value;
+                    var right = match.Groups[3].Value;
+
+                    var indexes = string.Concat(left, right)
+                        .Where(char.IsDigit)
+                        .Select(s => s - '0');
+
+                    var result = new StringBuilder();
+
+
+                    //.Select(s => s - '0');
+                
+                    //   =
+
+                    //foreach (var index in indexes)
+                    //{
+                    //    var digit = index - '0';
+                    //}
+
+                    foreach (var index in indexes)
+                    {
+                        if (index < 0 || index >= message.Length)
+                        {
+                            result.Append(' ');
+                        }
+                        else
+                        {
+                            result.Append(message[index]);
+                        }
+                    }
+                    Console.WriteLine($"{message} == {result}");
+                }
+            }
+        }
+    }
+}
