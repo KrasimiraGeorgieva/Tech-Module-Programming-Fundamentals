447fc68c751a8697aa8c354821632dc72cfb9044
Krasimira Georgieva <krassi77bg@gmail.com>
2017-07-12 15:35:00 +0300

    Add 01. Poke Mon solution.

---
+++ All Exams Tasks Solutions/Poke Mon_01/01. Poke Mon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main()
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            int resultPower = pokePower;
+            decimal halfPower = 0.5m * pokePower;
+
+            while (pokePower >= distance)
+            {
+                count++;
+                pokePower -= distance;
+                if (pokePower == halfPower)
+                {
+                    if (exhaustionFactor > 0)
+                        pokePower = pokePower / exhaustionFactor;
+                }
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(count);
+        }
+    }
+}
